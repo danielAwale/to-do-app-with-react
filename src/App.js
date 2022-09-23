945c80eadc30602c418c00a1a8dd872dd160d0fe
danielAwale <danielmadhukarawale@gmail.com>
2022-09-23 12:05:42 -0700

    contain a function to handle submit default behaviour

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TodoForm from './Components/TodoForm';
 
 function App() {
   return (
     <div className="todoapp">
       <h1>To Do App: A practice app for React!</h1>
+      <TodoForm />
     </div>
   );
 }
